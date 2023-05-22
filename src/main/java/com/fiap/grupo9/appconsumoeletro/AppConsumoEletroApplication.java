e07fb91aa2ff40b5754a599bfa8c3bccee2c4cd2
Rodrigo <rodrigolima.18@hotmail.com>
2023-05-22 19:50:08 -0300

    início do projeto

---
+++ src/main/java/com/fiap/grupo9/appconsumoeletro/AppConsumoEletroApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.grupo9.appconsumoeletro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppConsumoEletroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppConsumoEletroApplication.class, args);
+    }
+
+}
