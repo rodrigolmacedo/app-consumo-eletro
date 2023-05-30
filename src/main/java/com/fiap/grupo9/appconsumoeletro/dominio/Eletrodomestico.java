76c6c33a47fe5f3ab09373bfcd0843b3cf80c86b
Kaique Ribeiro <ksilvaribeiro97@gmail.com>
2023-05-29 22:00:34 -0300

    Adicionando Classes Dominio Eletrodomestico,Endereço,Pessoa,Residencia

---
+++ src/main/java/com/fiap/grupo9/appconsumoeletro/dominio/Eletrodomestico.java
@@ -0,0 +1,32 @@
+package com.fiap.grupo9.appconsumoeletro.dominio;
+
+import java.time.LocalDateTime;
+
+public class Eletrodomestico {
+
+    String nome;
+    String modelo;
+    double potencia;
+    double voltagem;
+
+    double consumo;
+
+    LocalDateTime horarioConsumo;
+
+
+    public void calcularMédiaConsumoHorario(){
+
+     // consumo=  (potencia/1000)*tempo;
+      //calcularMédiaConsumoHorario = consumo * ()
+
+    }
+
+
+
+
+
+
+
+
+
+}
--- src/main/java/com/fiap/grupo9/appconsumoeletro/dominio/Eletrodomesticos.java
+++
@@ -1,4 +0,0 @@
-package com.fiap.grupo9.appconsumoeletro.dominio;
-
-public class Eletrodomesticos {
-}
