f58ec72d93fcd449234c007c343b8f574e179a85
Rodrigo <rodrigolima.18@hotmail.com>
2023-05-30 21:28:07 -0300

    adicionando swagger

--- src/main/java/com/fiap/grupo9/appconsumoeletro/AppConsumoEletroApplication.java
+++ src/main/java/com/fiap/grupo9/appconsumoeletro/AppConsumoEletroApplication.java
@@ -1,7 +1,16 @@
 package com.fiap.grupo9.appconsumoeletro;
 
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppConsumoEletroApplication {
@@ -10,4 +19,11 @@ public class AppConsumoEletroApplication {
         SpringApplication.run(AppConsumoEletroApplication.class, args);
     }
 
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("ApiConsumoEletronico API"));
+    }
 }
