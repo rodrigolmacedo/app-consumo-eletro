6047f9d8e80d02beb1bfdf5ed731acb1c0a11f31
Rodrigo <7695045+rodrigolmacedo@users.noreply.github.com>
2023-05-31 22:10:57 -0300

    map teste

--- src/main/java/com/fiap/grupo9/appconsumoeletro/controllers/HomeController.java
+++ src/main/java/com/fiap/grupo9/appconsumoeletro/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/teste")
     public ResponseEntity<String> teste(){
         return ResponseEntity.ok("Teste");
     }
