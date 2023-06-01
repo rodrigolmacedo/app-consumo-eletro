b269a393010bd62a5d9477d42fc46e0bb455249b
Rodrigo <7695045+rodrigolmacedo@users.noreply.github.com>
2023-05-31 22:08:31 -0300

    mudando map

--- src/main/java/com/fiap/grupo9/appconsumoeletro/controllers/HomeController.java
+++ src/main/java/com/fiap/grupo9/appconsumoeletro/controllers/HomeController.java
@@ -4,9 +4,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController
 public class HomeController {
-    @GetMapping
+    @GetMapping("/")
     public ResponseEntity<String> teste(){
         return ResponseEntity.ok("Teste");
     }
